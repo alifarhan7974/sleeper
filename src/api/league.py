acdebda6bf223dc10e32634d675b8d4e0a857f21
Farhan Ali <farhanalicar@gmail.com>
2025-09-18 14:52:34 -0700

    change yml and fixed league.py

--- src/api/league.py
+++ src/api/league.py
@@ -1,17 +1,15 @@
 from api.utils import get, build_url
 from api.path_constants import BASE_URL, MATCHUPS, ROSTERS, TRADED_PICKS, USERS, VERSION, LEAGUE
 
+class League:
 
-
-class League: 
-
-    def __init__(self, league_id: int): 
+    def __init__(self, league_id: int):
         self.league_id = league_id
 
     def __str__(self): 
         return f"League id: {self.league_id}"
 
-    def get_league(self) -> dict: 
+    def get_league(self) -> dict:
         """Makes an get request to 
         https://api.sleeper.app/v1/league/<league_id>"""
 
@@ -19,27 +17,26 @@ class League:
         return get(full_url)
 
 
-    def get_league_rosters(self) -> dict: 
+    def get_league_rosters(self) -> dict:
         """Makes an get request to 
         https://api.sleeper.app/v1/league/<league_id>/rosters"""
 
         full_url = build_url(BASE_URL, VERSION, LEAGUE, self.league_id, ROSTERS)
         return get(full_url)
 
-    def get_league_users(self) -> dict: 
+    def get_league_users(self) -> dict:
         """Makes an get request to 
         https://api.sleeper.app/v1/league/<league_id>/users"""
 
         full_url = build_url(BASE_URL, VERSION, LEAGUE, self.league_id, USERS)
         return get(full_url)
 
-    def get_league_matchups(self, week: int) -> dict: 
+    def get_league_matchups(self, week: int) -> dict:
         """Makes an get request to 
         https://api.sleeper.app/v1/league/<league_id>/matchups/<week>"""
 
         full_url = build_url(BASE_URL, VERSION, LEAGUE, self.league_id, MATCHUPS, USERS, week)
         return get(full_url)
-    
 
     def get_traded_picks(self) -> dict: 
         """Makes a get request to 
